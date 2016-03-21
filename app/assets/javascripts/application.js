e4db91423977ccc54fcaf65bc988f57a7f7afdae
darammg <daramm.g@gmail.com>
2016-03-21 12:24:41 +0900

    ga 추가

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,17 @@
 //= require turbolinks
 //= require_tree .
 
+
+// ga 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-75373901-1', 'auto');
+ga('send', 'page view');
+// ga 
+
 function nearBottomOfPage() {
   return scrollDistanceFromBottom() < 150;
 }
