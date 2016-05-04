36ed7a8dc437b597ebb68d045ea24ece33e94d48
darammg <daramm.g@gmail.com>
2016-05-04 20:59:04 +0900

    이미지 리네임하던거 다시 롤백

--- app/models/ask_deal.rb
+++ app/models/ask_deal.rb
@@ -7,11 +7,4 @@ class AskDeal < ActiveRecord::Base
   validates_attachment_size :image, :less_than => 20.megabytes
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/pjpeg', 'image/pjpeg', 'image/png', 'image/jpg', 'image/gif', 'application/octet-stream']
   
-  before_create :rename_file
-  
-  def rename_file
-    charset = %w{ 2 3 4 6 7 9 a b c d e f g h i j k l m n o p q r s t v w x y z}
-    self.image_file_name = (0...6).map{ charset.to_a[rand(charset.size)] }.join + "." +  self.image_file_name.split(".").last 
-  end
-  
 end
