c8acd113147650a06c38edc917975fe81895995e
darammg <daramm.g@gmail.com>
2016-05-04 18:04:51 +0900

    검색관련 중복 이슈 해결

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,8 +19,8 @@ class HomeController < ApplicationController
         hash_tags = HashTag.where("keyword like ?", "%#{params[:keyword]}%" )
         @asks = Ask.where(:id => hash_tags.map(&:ask_id) ).page(params[:page]).per(Ask::ASK_PER).order("id desc").as_json(:include => [:category, :user, :left_ask_deal, :right_ask_deal, :ask_complete])
       when "ask_deal"
-        ask_deal = AskDeal.find_by_id(params[:keyword])
-        @asks = Ask.where("left_ask_deal_id = ? OR right_ask_deal_id = ?", ask_deal.id, ask_deal.id).page(params[:page]).per(Ask::ASK_PER).order("id desc").as_json(:include => [:category, :user, :left_ask_deal, :right_ask_deal, :ask_complete]) if ask_deal
+        ask_deals = AskDeal.where("title like ?", "%#{keyword}%" )
+        @asks = Ask.where("left_ask_deal_id in (?) OR right_ask_deal_id in (?)", ask_deals.map(&:id), ask_deals.map(&:id)).page(params[:page]).per(Ask::ASK_PER).order("id desc").as_json(:include => [:category, :user, :left_ask_deal, :right_ask_deal, :ask_complete]) unless ask_deals.blank?
       when "brand"
         ask_deals = AskDeal.where("brand like ?", "%#{params[:keyword]}%" )
         @asks = Ask.where("left_ask_deal_id in (?) OR right_ask_deal_id in (?)", ask_deals.map(&:id), ask_deals.map(&:id)).page(params[:page]).per(Ask::ASK_PER).order("id desc").as_json(:include => [:category, :user, :left_ask_deal, :right_ask_deal, :ask_complete]) unless ask_deals.blank?
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,12 +10,12 @@ class SearchController < ApplicationController
     is_empty_result = false
     unless keyword.blank?
       users = User.where("string_id like ?", "%#{keyword}%") 
-      hash_tags = HashTag.where("keyword like ?", "%#{keyword}%")
-      ask_deals = AskDeal.where("title like ?", "%#{keyword}%" )
+      hash_tags = HashTag.where("keyword like ?", "%#{keyword}%").select(:keyword).distinct(:keyword)
+      ask_deal = true unless AskDeal.where("title like ?", "%#{keyword}%" ).blank?
       brand = true unless AskDeal.where("brand like ?", "%#{keyword}%" ).blank?
     end
     is_empty_result = true if users.blank? && hash_tags.blank? && ask_deals.blank? && brand.blank?
-    render :json => {:users => users, :hash_tags => hash_tags, :ask_deals => ask_deals, :brand => brand, :is_empty_result => is_empty_result}
+    render :json => {:users => users, :hash_tags => hash_tags, :ask_deal => ask_deal, :brand => brand, :is_empty_result => is_empty_result}
   end
   
 end
