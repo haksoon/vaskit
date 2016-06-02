a0bee6a4461c1240aa9abebe590753bf9789bcfb
junsikahn <junsikahn.leisure@gmail.com>
2016-06-02 23:52:09 +0900

    마이페이지 레이어로 변경11 - 재도전

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -99,6 +99,12 @@ class HomeController < ApplicationController
       }
       format.json {render :json => { :asks => @asks }}
     end
+    @my_ask_count = Ask.where(:user_id => current_user.id).count
+    @my_vote_count = Vote.where(:user_id => current_user.id).count
+    @my_comment_count = Comment.where(:user_id => current_user.id).count
+
+    @in_progress_count = Ask.where(:user_id => current_user.id, :be_completed => false).count
+    @alram_count = Alram.where(:user_id => current_user.id, :is_read => false).count
   end
 
 
