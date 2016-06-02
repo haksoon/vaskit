f8f6bf29bbb269ab25847946a11328abb6042081
junsikahn <junsikahn.leisure@gmail.com>
2016-06-02 23:50:15 +0900

    마이페이지 레이어로 변경10 - 원상복구

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -122,13 +122,4 @@ class HomeController < ApplicationController
     @user_categories = UserCategory.where(:user_id => current_user.id).map(&:category_id) if current_user
   end
 
-  def user
-    @my_ask_count = Ask.where(:user_id => current_user.id).count
-    @my_vote_count = Vote.where(:user_id => current_user.id).count
-    @my_comment_count = Comment.where(:user_id => current_user.id).count
-
-    @in_progress_count = Ask.where(:user_id => current_user.id, :be_completed => false).count
-    @alram_count = Alram.where(:user_id => current_user.id, :is_read => false).count
-  end
-
 end
