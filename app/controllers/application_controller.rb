ee2670ecea36380d35b6195bc8883be3eb493e96
junsikahn <junsikahn.leisure@gmail.com>
2016-06-05 06:55:53 +0900

    헤더 통합 시도 - 실패

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -90,13 +90,5 @@ class ApplicationController < ActionController::Base
     end
     ret.split(",")[0]
   end
-
-  def user
-    @my_ask_count = Ask.where(:user_id => current_user.id).count
-    @my_vote_count = Vote.where(:user_id => current_user.id).count
-    @my_comment_count = Comment.where(:user_id => current_user.id).count
-    @in_progress_count = Ask.where(:user_id => current_user.id, :be_completed => false).count
-    @alram_count = Alram.where(:user_id => current_user.id, :is_read => false).count
-  end
-
+  
 end
