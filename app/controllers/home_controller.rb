b5e9c1af229cdf9ff0a0fde2c8dfdbb55d25f2ff
junsikahn <junsikahn.leisure@gmail.com>
2016-06-14 00:59:09 +0900

    AJS 컨트롤러 연습

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,9 +19,8 @@ class HomeController < ApplicationController
 
     case params[:type]
       when "user"
-        keyword_obj = User.where("string_id like ?", "%#{params[:keyword]}%").select(:string_id).distinct(:string_id)
-        keyword = keyword_obj.string_id
-        flash[:keyword] = keyword #AJS추가
+        @user_string_id = User.where(:id => params[:keyword]) #AJS추가
+        flash[:keyword] = @user_string_id.string_id #AJS추가
         @asks = Ask.where(:user_id => params[:keyword]).page(params[:page]).per(Ask::ASK_PER).order("id desc").as_json(:include => [:category, :user, :left_ask_deal, :right_ask_deal, :ask_complete])
       when "hash_tag"
         flash[:keyword] = params[:keyword] #AJS추가
