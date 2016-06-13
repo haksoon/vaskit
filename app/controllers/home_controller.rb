90032d8a2788ea3fced75b3d4e8c6aed6ae69f62
junsikahn <junsikahn.leisure@gmail.com>
2016-06-13 22:16:42 +0900

    AJS 컨트롤러 연습

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,6 +19,7 @@ class HomeController < ApplicationController
 
     case params[:type]
       when "user"
+        @keyword = params[:keyword]
         @asks = Ask.where(:user_id => params[:keyword]).page(params[:page]).per(Ask::ASK_PER).order("id desc").as_json(:include => [:category, :user, :left_ask_deal, :right_ask_deal, :ask_complete])
       when "hash_tag"
         hash_tags = HashTag.where("keyword like ?", "%#{params[:keyword]}%" )
@@ -39,7 +40,6 @@ class HomeController < ApplicationController
         @asks = Ask.where(:user_id => current_user.id, :be_completed => false).page(params[:page]).per(Ask::ASK_PER).order("id desc").as_json(:include => [:category, :user, :left_ask_deal, :right_ask_deal, :ask_complete])
       when "none" #통합 검색
         keyword = params[:keyword]
-        render text: keyword
         user_ask_ids = Ask.where(:user_id => User.where("string_id like ?", "%#{keyword}%").pluck(:id)).pluck(:id)
         hash_tag_ask_ids = Ask.where(:id => HashTag.where("keyword like ?", "%#{keyword}%" ).pluck(:ask_id) ).pluck(:id)
         title_ask_deal_ids = AskDeal.where("title like ?", "%#{keyword}%" ).pluck(:id)
