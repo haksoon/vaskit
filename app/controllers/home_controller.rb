078114c041558ea01cd4365655125349f4a81cd0
junsikahn <junsikahn.leisure@gmail.com>
2016-06-14 01:41:24 +0900

    AJS 컨트롤러 연습

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,7 +19,6 @@ class HomeController < ApplicationController
 
     case params[:type]
       when "user"
-        flash[:keyword] = User.where(:id => params[:keyword]) #AJS추가
         @asks = Ask.where(:user_id => params[:keyword]).page(params[:page]).per(Ask::ASK_PER).order("id desc").as_json(:include => [:category, :user, :left_ask_deal, :right_ask_deal, :ask_complete])
       when "hash_tag"
         flash[:keyword] = params[:keyword] #AJS추가
