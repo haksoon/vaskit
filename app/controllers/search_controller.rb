7c1108cec12c5d509a73fcd9e7e55c392fcc4957
junsikahn <junsikahn.leisure@gmail.com>
2016-06-14 11:36:55 +0900

    AJS 컨트롤러 연습

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 # coding : utf-8
 class SearchController < ApplicationController
-  
+
   def get_keyword
     keyword = params[:keyword]
     users = []
@@ -9,7 +9,7 @@ class SearchController < ApplicationController
     brand = false
     is_empty_result = false
     unless keyword.blank?
-      users = User.where("string_id like ?", "%#{keyword}%") 
+      users = User.where("string_id like ?", "%#{keyword}%").select(:string_id).distinct(:string_id)
       hash_tags = HashTag.where("keyword like ?", "%#{keyword}%").select(:keyword).distinct(:keyword)
       ask_deal = true unless AskDeal.where("title like ?", "%#{keyword}%" ).blank?
       brand = true unless AskDeal.where("brand like ?", "%#{keyword}%" ).blank?
@@ -17,5 +17,5 @@ class SearchController < ApplicationController
     is_empty_result = true if users.blank? && hash_tags.blank? && ask_deals.blank? && brand.blank?
     render :json => {:users => users, :hash_tags => hash_tags, :ask_deal => ask_deal, :brand => brand, :is_empty_result => is_empty_result}
   end
-  
+
 end
