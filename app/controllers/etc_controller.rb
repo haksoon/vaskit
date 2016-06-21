ad6b9414ee4b2568a17471d40ccd94ec97f97cae
junsikahn <junsikahn.leisure@gmail.com>
2016-06-22 00:40:34 +0900

    AJS 질문하기 화면으로 DB 연습

--- app/controllers/etc_controller.rb
+++ app/controllers/etc_controller.rb
@@ -17,13 +17,13 @@ class EtcController < ApplicationController
   def create_inquiry
     Inquiry.create(:user_id => current_user.id, :message => params[:message], :contact => params[:contact])
     render :json => {:status => "success" }
+    render :layout => "layout_empty"
   end
 
   def user
     @my_ask_count = Ask.where(:user_id => current_user.id).count
     @my_vote_count = Vote.where(:user_id => current_user.id).count
     @my_comment_count = Comment.where(:user_id => current_user.id).count
-
     @in_progress_count = Ask.where(:user_id => current_user.id, :be_completed => false).count
     @alram_count = Alram.where(:user_id => current_user.id, :is_read => false).count
   end
