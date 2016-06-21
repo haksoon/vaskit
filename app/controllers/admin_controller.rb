6c891aba7d44a097ab1791d478d44acfc46449dc
junsikahn <junsikahn.leisure@gmail.com>
2016-06-21 23:55:59 +0900

    AJS 어드민 페이지 레이아웃 리네임

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,7 @@
 # coding : utf-8
 class AdminController < ApplicationController
   before_filter :auth_admin
-  
+
   def index
     @notices = Notice.all.order("id desc")
     category_id = params[:category_id]
@@ -11,32 +11,32 @@ class AdminController < ApplicationController
         @asks = Ask.where(:category_id => category_id).order("id desc")
       else
         @asks = Ask.where(:category_id => category_id).where("id not in (?)", @rank_asks.map(&:ask_id)).order("id desc")
-      end 
+      end
       @category = Category.find(category_id)
     else
       @rank_asks = RankAsk.where(:category_id => nil).order("ranking asc")
       if @rank_asks.blank?
         @asks = Ask.all.order("id desc")
       else
-        @asks = Ask.where("id not in (?)", @rank_asks.map(&:ask_id)).order("id desc")   
+        @asks = Ask.where("id not in (?)", @rank_asks.map(&:ask_id)).order("id desc")
       end
     end
     @categories = Category.all
     @tables = ActiveRecord::Base.connection.tables
     @tables = @tables - ["schema_migrations"]
-    render :layout => "empty"
-  end 
-  
-  
+    render :layout => "admin_layout"
+  end
+
+
   def table
     @tables = ActiveRecord::Base.connection.tables
     @tables = @tables - ["schema_migrations"]
     tableModel = params[:table_name].classify.constantize
     @record_names = tableModel.columns.map(&:name)
     @records = tableModel.all.order("id desc")
-    render :layout => "empty"
+    render :layout => "admin_layout"
   end
-  
+
   def submit_rank_ask
     ask_id = params[:ask_id]
     ranking = params[:ranking]
@@ -47,15 +47,15 @@ class AdminController < ApplicationController
       rank_ask.update(:ask_id => ask_id)
     else
       RankAsk.create(:ask_id => ask_id, :ranking => ranking, :category_id => category_id)
-    end 
+    end
     render :json => {:status => "success"}
   end
-  
+
   def delete_rank_ask
     RankAsk.find(params[:rank_ask_id]).delete
     render :json => {:status => "success"}
   end
-  
+
   def create_notice
     notice = Notice.create(:title => params[:title], :message => params[:message])
     User.where(:receive_notice_email => true).each do |user|
@@ -63,5 +63,5 @@ class AdminController < ApplicationController
     end
     render :json => {:status => "success"}
   end
-  
+
 end
