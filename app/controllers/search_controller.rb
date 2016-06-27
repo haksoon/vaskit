2dac61eea41be81268e7d0f2689e0c741eeaeb9c
junsikahn <junsikahn.leisure@gmail.com>
2016-06-27 21:40:04 +0900

    AJS 유저 검색 쿼리 테스트

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,7 +14,7 @@ class SearchController < ApplicationController
       brand = true unless AskDeal.where("brand like ?", "%#{keyword}%" ).blank?
       # users = User.where("string_id like ?", "%#{keyword}%")
       asks = Ask.select(:user_id).distinct(:user_id)
-      users = User.where("string_id like ?", "#{keyword}%", :id => asks.map(&:user_id)).select(:string_id).distinct(:string_id) #AJS추가(수정) - 유저의 경우에도 string_id가 keyword가 되도록 로직 변경
+      users = User.where("string_id like ?", "#{keyword}%").where(:id => asks.map(&:user_id)).select(:string_id).distinct(:string_id) #AJS추가(수정) - 유저의 경우에도 string_id가 keyword가 되도록 로직 변경
     end
     is_empty_result = true if users.blank? && hash_tags.blank? && ask_deals.blank? && brand.blank?
     render :json => {:users => users, :hash_tags => hash_tags, :ask_deal => ask_deal, :brand => brand, :is_empty_result => is_empty_result}
