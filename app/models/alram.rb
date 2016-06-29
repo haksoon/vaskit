d878005dd96261ac44d96492cce3ab31498a90ee
junsikahn <junsikahn.leisure@gmail.com>
2016-06-30 02:15:35 +0900

    AJS 마이페이지 구조 통합

--- app/models/alram.rb
+++ app/models/alram.rb
@@ -1,3 +1,4 @@
 class Alram < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, :class_name => 'SendUser', :foreign_key => 'send_user_id'
+  belongs_to :user, :class_name => 'OwnSendUser', :foreign_key => 'ask_owner_user_id'
 end
