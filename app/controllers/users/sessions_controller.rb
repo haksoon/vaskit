6b1e7066633d01506b97e2426d98c4652329e715
junsikahn <junsikahn.leisure@gmail.com>
2016-06-30 02:04:51 +0900

    AJS 마이페이지 구조 통합

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,9 +12,8 @@ class Users::SessionsController < Devise::SessionsController
       in_progress_count = Ask.where(:user_id => current_user.id, :be_completed => false).count #AJS추가
       alram_count = Alram.where(:user_id => current_user.id, :is_read => false).count #AJS추가
       @alrams = Alram.where(:user_id => current_user.id).order("updated_at desc").limit(15)
-      @users = User.all.select(:id, :string_id)
     end
-    render :json => {:current_user_string_id => current_user_string_id, :my_ask_count => my_ask_count, :my_vote_count => my_vote_count, :my_comment_count => my_comment_count, :in_progress_count => in_progress_count, :alram_count => alram_count, :alrams => @alrams, :users => @users}
+    render :json => {:current_user_string_id => current_user_string_id, :my_ask_count => my_ask_count, :my_vote_count => my_vote_count, :my_comment_count => my_comment_count, :in_progress_count => in_progress_count, :alram_count => alram_count, :alrams => @alrams}
   end
 
   def create
