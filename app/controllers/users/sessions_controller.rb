8bcc58223205dbdabb06e36d7276986c87ae0879
junsikahn <junsikahn.leisure@gmail.com>
2016-06-30 00:30:21 +0900

    AJS 마이페이지 구조 통합

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,14 +5,14 @@ class Users::SessionsController < Devise::SessionsController
   #AJS추가
   def show
     if current_user
-      current_user_id = User.where(:id => current_user.id).select(:string_id)
+      current_user_string_id = User.where(:id => current_user.id).select(:string_id)
       my_ask_count = Ask.where(:user_id => current_user.id).count #AJS추가
       my_vote_count = Vote.where(:user_id => current_user.id).count #AJS추가
       my_comment_count = Comment.where(:user_id => current_user.id).count #AJS추가
       in_progress_count = Ask.where(:user_id => current_user.id, :be_completed => false).count #AJS추가
       alram_count = Alram.where(:user_id => current_user.id, :is_read => false).count #AJS추가
     end
-    render :json => {:current_user_id => current_user_id, :my_ask_count => my_ask_count, :my_vote_count => my_vote_count, :my_comment_count => my_comment_count, :in_progress_count => in_progress_count, :alram_count => alram_count}
+    render :json => {:current_user_string_id => current_user_string_id, :my_ask_count => my_ask_count, :my_vote_count => my_vote_count, :my_comment_count => my_comment_count, :in_progress_count => in_progress_count, :alram_count => alram_count}
   end
 
   def create
