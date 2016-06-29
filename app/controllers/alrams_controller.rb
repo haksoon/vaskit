52498b67e3385e3a6e600a09104896aedaae2e81
junsikahn <junsikahn.leisure@gmail.com>
2016-06-30 03:03:47 +0900

    AJS 마이페이지 구조 통합

--- app/controllers/alrams_controller.rb
+++ app/controllers/alrams_controller.rb
@@ -1,10 +1,19 @@
 # coding : utf-8
 class AlramsController < ApplicationController
-  
+
   def index
     @alrams = Alram.where(:user_id => current_user.id).order("updated_at desc").limit(15)
   end
-  
+
+  def read
+    Alram.where(:id => params[:id], :is_read => false).each do |alram|
+      alram.record_timestamps = false #updated_at 안바뀌게
+      alram.update(:is_read => true)
+      alram.record_timestamps = true #updated_at 안바뀌게
+    end
+    render :json => {:status => "success"}
+  end
+
   def all_read
     Alram.where(:user_id => current_user.id, :is_read => false).each do |alram|
       alram.record_timestamps = false #updated_at 안바뀌게
@@ -13,5 +22,5 @@ class AlramsController < ApplicationController
     end
     render :json => {:status => "success"}
   end
-  
+
 end
