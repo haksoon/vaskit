5e915eb6bb42202f6c5cab280ca40cfbe2854f23
junsikahn <junsikahn.leisure@gmail.com>
2016-06-30 03:31:19 +0900

    AJS 마이페이지 구조 통합

--- app/controllers/alrams_controller.rb
+++ app/controllers/alrams_controller.rb
@@ -11,7 +11,8 @@ class AlramsController < ApplicationController
       alram.update(:is_read => true)
       alram.record_timestamps = true #updated_at 안바뀌게
     end
-    render :json => {:status => "success"}
+    is_new_alram = Alram.where(:user_id => current_user.id, :is_read => false).blank?
+    render :json => {:status => "success", :is_new_alram => is_new_alram}
   end
 
   def all_read
