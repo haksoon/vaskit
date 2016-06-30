15a7398cd540d5f606d9a545773ebdfe6490ee87
junsikahn <junsikahn.leisure@gmail.com>
2016-06-30 18:44:07 +0900

    AJS 마이페이지 구조 통합

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -13,11 +13,13 @@ class Users::SessionsController < Devise::SessionsController
       alram_count = Alram.where(:user_id => current_user.id, :is_read => false).count
       is_new_alram = Alram.where(:user_id => current_user.id, :is_read => false).blank?
       @alrams = Alram.where(:user_id => current_user.id).order("updated_at desc").limit(15)
+      @owner_users = []
+      @send_users = []
       @alrams.each do |alram|
-        @owner_users = User.where(:id => alram.ask_owner_user_id)
-      end
-      @alrams.each do |alram|
-        @send_users = User.where(:id => alram.send_user_id)
+        owner_user = User.where(:id => alram.ask_owner_user_id)
+        send_user = User.where(:id => alram.send_user_id)
+        @owner_users.push(owner_user)
+        @send_users.push(send_user)
       end
     end
     render :json => {:current_user_string_id => current_user_string_id, :my_ask_count => my_ask_count, :my_vote_count => my_vote_count, :my_comment_count => my_comment_count, :in_progress_count => in_progress_count, :alram_count => alram_count,
