145d29d9b8cd48b0c87d8d81074732a32494159c
junsikahn <junsikahn.leisure@gmail.com>
2016-07-22 22:51:32 +0900

    AJS 비밀번호 재설정 이메일 추가

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,14 @@
 # coding : utf-8
 class UserMailer < ActionMailer::Base
+  include Rails.application.routes.url_helpers
   default from: 'notice@vaskit.kr'
 
   def send_notice(user, notice)
     @message = notice.message
     mail(to: user.email, subject: notice.title).deliver
+  end
+
+  def default_url_options
+    Rails.application.config.action_mailer.default_url_options
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,6 @@ Rails.application.configure do
     :password => 'vaskit1234',#password
     :enable_starttls_auto => true
   }
-  config.action_mailer.default_url_options = { :host => 'vaskit.kr' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
