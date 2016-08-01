c968636ec88f43cf5b7e9f2e379613fd7f8f63b7
junsikahn <junsikahn.leisure@gmail.com>
2016-08-01 14:14:27 +0900

    AJS 댓글창 오류 수정

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
 
-  after_create :incr_ask_deal_comment_count
-  before_destroy :decr_ask_deal_comment_count
+  after_create :ask_deal_comment_count
+  after_update :ask_deal_comment_count
+  before_destroy :ask_deal_comment_count
 
-
-  def incr_ask_deal_comment_count
+  def ask_deal_comment_count
     ask = Ask.find_by_id(self.ask_id)
     if ask.left_ask_deal_id == self.ask_deal_id
       ask.left_ask_deal.update(:comment_count => Comment.where(:ask_deal_id => self.ask_deal_id, :is_deleted => 0).count )
@@ -14,13 +14,13 @@ class Comment < ActiveRecord::Base
     end
   end
 
-  def decr_ask_deal_comment_count
-    ask = Ask.find_by_id(self.ask_id)
-    if ask.left_ask_deal_id == self.ask_deal_id
-      ask.left_ask_deal.update(:comment_count => Comment.where(:ask_deal_id => self.ask_deal_id).count - 1 )
-    elsif ask.right_ask_deal_id == self.ask_deal_id
-      ask.right_ask_deal.update(:comment_count => Comment.where(:ask_deal_id => self.ask_deal_id).count - 1 )
-    end
-  end
+  # def decr_ask_deal_comment_count
+  #   ask = Ask.find_by_id(self.ask_id)
+  #   if ask.left_ask_deal_id == self.ask_deal_id
+  #     ask.left_ask_deal.update(:comment_count => Comment.where(:ask_deal_id => self.ask_deal_id, :is_deleted => 0).count )
+  #   elsif ask.right_ask_deal_id == self.ask_deal_id
+  #     ask.right_ask_deal.update(:comment_count => Comment.where(:ask_deal_id => self.ask_deal_id, :is_deleted => 0).count )
+  #   end
+  # end
 
 end
