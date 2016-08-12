60412ee6d4dd8df86ef1a6e68ae7f48617e522fd
junsikahn <junsikahn.leisure@gmail.com>
2016-08-13 02:34:02 +0900

    AJS 댓글 입력창 띄어쓰기

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -448,6 +448,21 @@ $.fn.extend({
     }
 });
 
+$.fn.selectRange = function(start, end) {
+  return this.each(function() {
+    if(this.setSelectionRange) {
+      this.focus();
+      this.setSelectionRange(start, end);
+    } else if(this.createTextRange) {
+      var range = this.createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', end);
+      range.moveStart('character', start);
+      range.select();
+    }
+  });
+};
+
 $( document ).ready(function() {
   $("select").on("change",function(){
     if( $(this).val() != "" ) {
