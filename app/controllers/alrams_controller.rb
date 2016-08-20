ef2d8a175df9d9481de64d2c365f953fbf743d10
junsikahn <junsikahn.leisure@gmail.com>
2016-08-20 23:30:05 +0900

    AJS 알림피드 읽음 방식 개선, 이미지 확대 레이어 오류 수정 및 개선

--- app/controllers/alrams_controller.rb
+++ app/controllers/alrams_controller.rb
@@ -7,12 +7,13 @@ class AlramsController < ApplicationController
 
   #AJS추가
   def read
+    ask = Ask.find_by_id(params[:ask_id]).as_json(:include => [:category, :user, :left_ask_deal, :right_ask_deal, :ask_complete, {:comments => {:include => :user}} ])
     Alram.where(:id => params[:id], :is_read => false).each do |alram|
       alram.record_timestamps = false #updated_at 안바뀌게
       alram.update(:is_read => true)
       alram.record_timestamps = true #updated_at 안바뀌게
     end
-    render :json => {:status => "success"}
+    render :json => {:status => "success", :ask => ask}
   end
 
   def all_read
