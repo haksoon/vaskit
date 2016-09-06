16e1a6697ce164f93a0e61e6e7b2034706134247
junsikahn <junsikahn.leisure@gmail.com>
2016-09-07 05:58:58 +0900

    AJS share_log 개선

--- app/controllers/share_logs_controller.rb
+++ app/controllers/share_logs_controller.rb
@@ -5,9 +5,8 @@ class ShareLogsController < ApplicationController
 
   # POST /share_logs.josn
   def create
-    if current_user
-      ShareLog.create(:user_id => current_user.id, :channel => params[:channel], :ask_id => params[:ask_id])
-    end
+    user_id = current_user ? current_user.id : nil
+    ShareLog.create(:user_id => user_id, :channel => params[:channel], :ask_id => params[:ask_id])
     render :json => { :status => "success"}
   end
 
