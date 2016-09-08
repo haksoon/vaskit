ec7c2748c3a4886932696ad85440a4de6d9ec23f
junsikahn <junsikahn.leisure@gmail.com>
2016-09-08 22:15:54 +0900

    AJS 웰컴 메일 개선

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,12 @@
 # coding : utf-8
 class UserMailer < ActionMailer::Base
-  default from: 'notice@vaskit.kr'
+  default from: %{"VASKIT" <notice@vaskit.kr>}
 
   def welcome_email(user)
     @user = user
+    @ask_count = Ask.all.count
+    @vote_count = Vote.all.count
+    @commnent_count = Comment.all.count
     mail(to: user.email, subject: "[VASKIT] #{user.string_id}님, 환영합니다!").deliver
   end
 
