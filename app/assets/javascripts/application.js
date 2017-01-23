98fba215c4497375504c4533a6abe63e045f9166
junsikahn <junsikahn.leisure@gmail.com>
2017-01-24 07:24:49 +0900

    AJS 안드로이드 구버전 앱 업데이트 화면 변경

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -97,10 +97,7 @@ function setGCM(key, device_id, app_ver) {
                 <div class='align_middle' style='color: #fff;'>\
                   <img src='/images/logo/logo_landing.png' style='width: 180px; margin-bottom: -60px;'>\
                   <br>완전히 달라진 VASKIT!\
-                  <br>앱을 업데이트해주세요 🙈<br>\
-                  <a href='https://play.google.com/store/apps/details?id=com.vaskit.msh.vaskit'>\
-                    <img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png' style='width: 220px;'>\
-                  </a>\
+                  <br>앱을 업데이트해주세요 🙈\
                 </div>\
               </div>";
   $("body").html(html);
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -21,24 +21,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
 
     if data[:email].blank?
-      render :json => {:status => "blank_email"}
+      render json: {status: "blank_email"}
     elsif data[:password].blank?
-      render :json => {:status => "blank_password"}
+      render json: {status: "blank_password"}
     elsif !data[:email].match(reg)
-      render :json => {:status => "not_email"}
+      render json: {status: "not_email"}
     elsif !User.find_for_database_authentication(email: data[:email]).blank?
-      render :json => {:status => "already_exist_email"}
+      render json: {status: "already_exist_email"}
     elsif data[:password].length < 8
-      render :json => {:status => "not_enough_password"}
+      render json: {status: "not_enough_password"}
     elsif data[:password] != data[:password_confirmation]
-      render :json => {:status => "password_confirm_error"}
+      render json: {status: "password_confirm_error"}
     else
       if data[:birthday].blank? && (data["birthday(1i)"] == nil || data["birthday(2i)"] == nil || data["birthday(3i)"] == nil || data["birthday(1i)"] == "" || data["birthday(2i)"] == "" || data["birthday(3i)"] == "")
-        render :json => {:status => "birthday_not_selected"}
+        render json: {status: "birthday_not_selected"}
       elsif data[:gender] == nil || data[:gender] == ""
-        render :json => {:status => "gender_not_selected"}
+        render json: {status: "gender_not_selected"}
       elsif data[:agree_access_term] != "1"
-        render :json => {:status => "agree_access_term"}
+        render json: {status: "agree_access_term"}
       else
         data[:birthday] = Date.new(data["birthday(1i)"].to_i, data["birthday(2i)"].to_i, data["birthday(3i)"].to_i).strftime("%m/%d/%Y") if data[:birthday].blank?
 
@@ -46,25 +46,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
         data[:remember_me] = true
 
         build_resource({
-          :email => data[:email],
-          :password => data[:password],
-          :birthday => data[:birthday],
-          :gender => data[:gender],
-          :string_id => data[:string_id],
-          :sign_up_type => data[:sign_up_type],
-          :facebook_id => data[:facebook_id],
-          :name => data[:name],
-          :remember_me => data[:remember_me],
-          :agree_access_term => data[:agree_access_term],
-          :avatar => data[:avatar]
+          email: data[:email],
+          password: data[:password],
+          birthday: data[:birthday],
+          gender: data[:gender],
+          string_id: data[:string_id],
+          sign_up_type: data[:sign_up_type],
+          facebook_id: data[:facebook_id],
+          name: data[:name],
+          remember_me: data[:remember_me],
+          agree_access_term: data[:agree_access_term],
+          avatar: data[:avatar]
         })
 
         resource.save
         if resource.persisted?
           sign_up(resource_name, resource)
-          render :json => {:status => "success", :string_id => data[:string_id]}
+          render json: {status: "success", string_id: data[:string_id]}
 
-          user = User.find_by(:email => data[:email])
+          user = User.find_by(email: data[:email])
           UserMailer.delay.welcome_email(user)
           AdminMailer.delay.signup_submitted(user)
         end
@@ -75,7 +75,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /users/check_email.json
   def check_email
     is_new_email = true if User.find_by_email(params[:email]).blank?
-    render :json => {:is_new_email => is_new_email}
+    render json: {is_new_email: is_new_email}
   end
 
   protected
