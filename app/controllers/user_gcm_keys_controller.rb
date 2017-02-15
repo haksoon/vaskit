13f23df86b65b5a762401ed23f6d868acacb2ae0
junsikahn <junsikahn.leisure@gmail.com>
2017-02-15 17:04:49 +0900

    AJS FCM 토큰 관련 오류 수정

--- app/controllers/user_gcm_keys_controller.rb
+++ app/controllers/user_gcm_keys_controller.rb
@@ -1,51 +1,35 @@
 class UserGcmKeysController < ApplicationController
   # POST /user_gcm_keys.json
   def create
+    user_id = current_user ? current_user.id : nil
     gcm_key = params[:gcm_key]
     device_id = params[:device_id]
     app_ver = params[:app_ver]
 
-    user_gcm_key = UserGcmKey.find_by(device_id: device_id)
-    if current_user
-      if user_gcm_key.nil?
-        user_gcm_key = UserGcmKey.find_by(gcm_key: gcm_key)
-        if user_gcm_key.nil?
-          status = 'log_in'
-          user_gcm_key = UserGcmKey.create(user_id: current_user.id,
-                                           gcm_key: gcm_key,
-                                           device_id: device_id,
-                                           app_ver: app_ver)
-        else
-          status = 'device_changed'
-          user_gcm_key.update(user_id: current_user.id,
-                              device_id: device_id,
-                              app_ver: app_ver)
-        end
-      elsif user_gcm_key.gcm_key != gcm_key || user_gcm_key.app_ver != app_ver
-        status = 'token_updated'
-        user_gcm_key.update(user_id: current_user.id,
-                            gcm_key: gcm_key,
-                            app_ver: app_ver)
-      else
-        status = 'token_existed'
-      end
-    else
-      # 로그아웃한 경우 또는 앱을 삭제했다가 다시 설치한 경우 기존 유저 정보를 제거함 (단, 앱 삭제 시점은 캐치할 수 없음)
-      status = 'log_out'
-      user_gcm_key = UserGcmKey.find_by(gcm_key: gcm_key) if user_gcm_key.nil?
-      user_gcm_key.update(user_id: nil) unless user_gcm_key.nil?
+    user_gcm_key = UserGcmKey.find_by(gcm_key: gcm_key)
+
+    if user_gcm_key.nil?
+      user_gcm_key = UserGcmKey.create(user_id: user_id,
+                                       gcm_key: gcm_key,
+                                       device_id: device_id,
+                                       app_ver: app_ver)
+    elsif user_gcm_key.user_id != user_id || user_gcm_key.gcm_key != gcm_key || user_gcm_key.device_id != device_id || user_gcm_key.app_ver != app_ver
+      user_gcm_key.update(user_id: user_id,
+                          gcm_key: gcm_key,
+                          device_id: device_id,
+                          app_ver: app_ver)
     end
 
     # 앱 뱃지 카운트 초기화
     registration_ids = []
     registration_ids << gcm_key
-    if status == 'log_out'
-      count = '0'
-    else
+    if current_user
       alarms = Alarm.where(user_id: current_user.id)
                     .order(updated_at: :desc)
                     .limit(20)
       count = alarms.pluck(:is_read).count(false).to_s
+    else
+      count = '0'
     end
 
     payload = {
